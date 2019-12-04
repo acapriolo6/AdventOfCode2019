81b82ff804c65560d04b2117faac6e995e1aa500
caa <caa@ti8m.ch>
2019-12-04 01:28:22 +0100

    Second part of the third day

--- src/Day03.java
+++ src/Day03.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.abs;
 
@@ -21,15 +22,28 @@ public class Day03 {
         }
         segments.get(0).retainAll(segments.get(1));
         Point point = segments.get(0).stream().filter(it -> !it.equals(new Point(0,0))).sorted((a, b) -> comparePoint(a, b)).findFirst().orElse(new Point(0, 0));
+        List<Integer> collect = segments.get(0).stream().map(it -> it.totalStep + findTotalStepInSet(segments.get(1), it)).collect(Collectors.toList());
+        Collections.sort(collect);
         System.out.println(calculateDistance(point));
+        System.out.println(collect.get(1));
     }
     private static int calculateDistance(Point a) {
         return abs(a.x) + abs(a.y);
     }
 
+    private static int findTotalStepInSet(Set<Point> set, Point point) {
+        return set.stream().filter(it -> it.equals(point)).findFirst().orElse(new Point(0, 0)).totalStep;
+    }
+
     private static int comparePoint(Point a, Point b) {
         if (calculateDistance(a) > calculateDistance(b) ) return 1;
         else if (calculateDistance(a) < calculateDistance(b) ) return -1;
+        return 0;
+    }
+
+    private static int compareSteps(Point a, Point b) {
+        if (b.totalStep > a.totalStep ) return 1;
+        else if (b.totalStep < a.totalStep ) return -1;
         return 0;
     }
 
@@ -46,32 +60,34 @@ public class Day03 {
         int startPoint = 0;
         int pointerI = startPoint;
         int pointerJ = startPoint;
+        int totalStep = 0;
         for (Step step: steps) {
+            totalStep = totalStep != 0 ? totalStep-1 : totalStep;
             int startY = pointerJ;
             int startX = pointerI;
             switch (step.direction){
                 case "U":
                     pointerJ += step.stepNumber;
                     while (startY <= pointerJ) {
-                        path.add(new Point(startX, startY++));
+                        path.add(new Point(startX, startY++, totalStep++));
                     }
                     break;
                 case "D":
                     pointerJ -= step.stepNumber;
                     while (startY >= pointerJ) {
-                        path.add(new Point(startX, startY--));
+                        path.add(new Point(startX, startY--, totalStep++));
                     }
                     break;
                 case "L":
                     pointerI -= step.stepNumber;
                     while (startX >= pointerI) {
-                        path.add(new Point(startX--, startY));
+                        path.add(new Point(startX--, startY, totalStep++));
                     }
                     break;
                 case "R":
                     pointerI += step.stepNumber;
                     while (startX <= pointerI) {
-                        path.add(new Point(startX++, startY));
+                        path.add(new Point(startX++, startY, totalStep++));
                     }
                     break;
             }
@@ -122,9 +138,17 @@ public class Day03 {
         int x;
         int y;
 
+        int totalStep;
+
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        public Point(int x, int y, int totalStep) {
+            this.x = x;
+            this.y = y;
+            this.totalStep = totalStep;
         }
 
         @Override
